7c13ceaaca11b7f53a4575f30fd77f6f94ea3b06
s20181102934 <s20181102934@admindeimac-38.local>
2019-06-19 20:47:01 +0800

    Initial Commit

---
+++ charming/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  charming
+//
+//  Created by s20181102934 on 2019/6/19.
+//  Copyright © 2019 tod new. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
