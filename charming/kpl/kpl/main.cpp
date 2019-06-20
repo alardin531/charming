fda040ec8a6f48f43bf4bb51078ae11cae592c58
s20181102934 <s20181102934@admindeiMac-38.local>
2019-06-20 10:29:46 +0800

    Update main.cpp

--- charming/kpl/kpl/main.cpp
+++ charming/kpl/kpl/main.cpp
@@ -10,9 +10,19 @@
 #include<fstream>
 using namespace std;
 int main(){
+    int a,b;
     ofstream huk("/Users/s20181102934/Desktop/timi.doc ");
+    ifstream rand("/Users/s20181102934/Desktop/qieshengxuan.txt");
+    rand>>a>>b;
+    cout<<"c ="<<a+b<<endl;
+    /*if(rand.is_open()){
+        //cout<<"aha you win"<<endl;
+        rand>>a>>b;
+        cout<<a<<"   "<<b<<endl;
+        rand.close();
+    }*/
     if(huk.is_open()){
-        huk<<"aha you win"<<endl;
+        huk<<a+b+90<<endl;
         huk.close();
     }
     return 0;
